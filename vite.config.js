93b235e979b39e642ff142830c95d3119e568f71
zeek1082 <jamespsmith1082@gmail.com>
2024-12-22 17:16:59 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  base: '/jasper-portfolio/',
+  build: {
+    outDir: 'docs'
   }
-});
+})
